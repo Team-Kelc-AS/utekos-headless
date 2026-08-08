3a5018c1122ecbd8c900e0c267323260e20dc612
utekos-brand <296267223+utekos-brand@users.noreply.github.com>
2026-08-08 14:42:49 +0200

    jo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -139,7 +139,7 @@ export const metadata: Metadata = {
     description:
       'Utekos er en merkevare som designer funksjonelt yttertøy for kompromissløs komfort og overlegen allsidighet. Perfekt for hytteliv, bobilferie, telttur, i båt og terrasseliv.',
     images: {
-      url: 'https://utekos.no/og-kate-linn-kikkert-master.png',
+      url: 'https://utekos.no/og-utekos_brand.jpg',
       width: 1200,
       height: 630,
       alt: 'To kvinner som koser seg utendørs på terrassen med varme komfortplagg fra Utekos.'
